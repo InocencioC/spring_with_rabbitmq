fbe5bf31211ebce5555e74d3cbdc9229fc547974
InocencioC <cordeiroturorias@gmail.com>
2023-02-16 17:27:28 +0100

    returning hashCode

--- src/main/java/com/example/demo/Sender.java
+++ src/main/java/com/example/demo/Sender.java
@@ -1,9 +1,23 @@
 package com.example.demo;
 
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.boot.autoconfigure.data.mongo.ReactiveStreamsMongoClientDependsOnBeanFactoryPostProcessor;
+
+
 public class Sender {
 
     public static void main (String[] args0) throws Exception {
         //Primeiro criar a conexão
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setPort(5672);
+
+        Connection connection = factory.newConnection();
+        System.out.println(connection.hashCode());
+
     }
 
 }
